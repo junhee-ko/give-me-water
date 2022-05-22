176d59dffa8bbe4bc4e5e11f9940d01079004480
junhee-ko <junheee.ko@gmail.com>
2022-05-22 22:58:07 +0900

    Remove importing PlantItem.css

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,6 +1,5 @@
 import React from "react";
-import './PlantItem.css'
-import GiveMeWaterModal from "./Modal";
+import GiveMeWaterModal from "./GiveMeWaterModal";
 
 const PlantItem = ({plant}) => {
 
