949afa55a42bfddc59c1a96a5393fa394b6b4ee7
junhee-ko <junheee.ko@gmail.com>
2022-05-22 22:57:08 +0900

    Add PlantItem.js

---
+++ src/components/PlantItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './PlantItem.css'
+import GiveMeWaterModal from "./Modal";
+
+const PlantItem = ({plant}) => {
+
+  return (
+    <div className="item">
+      <div className="ui small image">
+        <img alt={plant.name} src={plant.imageUrl}/>
+      </div>
+
+      <div className="middle aligned content">
+        <div className="header">
+          {plant.name}
+        </div>
+        <div className="description">
+          <p>물 주는 날 D-{plant.dDays}</p>
+        </div>
+        <div className="extra">
+          <div className="right floated">
+            <GiveMeWaterModal/>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default PlantItem
